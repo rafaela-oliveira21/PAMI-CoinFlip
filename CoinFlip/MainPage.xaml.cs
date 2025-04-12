dad4045358027d8ad7f84dc9203cffd448d0051a
rafaela-oliveira21 <rafalelaoliveira20@gmail.com>
2025-04-11 21:20:22 -0300

    CoinFlip

---
+++ CoinFlip/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace CoinFlip
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void FlipButton_Clicked(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int sorteio = random.Next(2);
+            //Animação();
+            if (sorteio == 0)
+            {
+                MoedaImage.Source = "cara.png";
+            }
+            else
+            {
+                MoedaImage.Source = "coroa.png";
+            }
+
+            if (SelecaoPicker.SelectedIndex == sorteio)
+            {
+                DisplayAlert("Parabéns", "Você venceu!", "Ok");
+            }
+            else
+            {
+                DisplayAlert("Se ferrou", "Você perdeu!", "Ok");
+            }
+        }
+    }
+
+}
